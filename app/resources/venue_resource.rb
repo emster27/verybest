c2cfea3824a17c3c8466432d8403eee7556513e7
anonymous anonymous <emi.rosanwo@kellogg.northwestern.edu>
2022-02-23 22:28:36 +0000

    Generate graphiti resources for venues

---
+++ app/resources/venue_resource.rb
@@ -0,0 +1,15 @@
+class VenueResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :state, :string
+  attribute :city, :string
+  attribute :neighborhood, :string
+  attribute :cuisine_id, :integer
+  attribute :user_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
